9eab038e50646aabfc43756ae2f7adb5f819d44f
Frederick Tetteh <frederick.tetteh@ontariotechu.net>
2023-01-11 17:31:25 -0500

    added board state and also react events

--- tictactoe/src/components/Square.jsx
+++ tictactoe/src/components/Square.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 
-export const Square= ({value}) => { //pass in a destructured value
- 
+export const Square= ({value, onClick}) => { //pass in a destructured value
+ console.log(value);
   return (
         
-        <button type='button'>{ value }</button>//passed an attribute to this button
+        <button type='button' className='square' onClick={onClick}//pass in the value of the square
+        >{ value}
+        
+        </button>//passed an attribute to this button
     
   )
 }
+
+//we can call childern props from here
